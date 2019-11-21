3251d2cfe0e5496c63856d5533430fb01e2759ad
Brett Shepherd <brettshepherd99@gmail.com>
2019-11-21 09:19:09 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,32 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { HttpClientModule, HttpClientJsonpModule } from "@angular/common/http";
+// import { HttpClientModule, HttpClientJsonpModule } from "@angular/common/http";
 import { AppRoutingModule } from "./routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { Store } from "./store";
+
+// Firebase
+import { AngularFireModule } from "@angular/fire";
+
+const config = {
+  apiKey: "<your-key>",
+  authDomain: "<your-project-authdomain>",
+  databaseURL: "<your-database-URL>",
+  projectId: "<your-project-id>",
+  storageBucket: "<your-storage-bucket>",
+  messagingSenderId: "<your-messaging-sender-id>"
+};
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    HttpClientJsonpModule,
-    BrowserAnimationsModule
+    // HttpClientModule,
+    // HttpClientJsonpModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(config)
   ],
   providers: [Store],
   bootstrap: [AppComponent]
