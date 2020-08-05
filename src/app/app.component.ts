773511aafd74b0de026cd95d7758f9caa3157a8d
Brett Shepherd <brettshepherd99@gmail.com>
2020-08-05 12:41:23 -0400

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,21 @@
 import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
-import { RouteAnimations } from "./animations";
 
 @Component({
   selector: "app-root",
   template: `
-    <div class="page" [@routeAnimation]="getDepth(myOutlet)">
+    <div class="page">
       <router-outlet #myOutlet="outlet"></router-outlet>
     </div>
   `,
   styleUrls: ["./app.component.sass"],
-  animations: RouteAnimations
+  // animations: RouteAnimations
 })
 export class AppComponent {
   //-------ROUTER ANIMS------
-  getDepth(outlet: RouterOutlet) {
-    let name = outlet.activatedRouteData["name"];
-    if (name) return name;
-    else return -1;
-  }
+  // [@routeAnimation]="getDepth(myOutlet)"
+  // getDepth(outlet: RouterOutlet) {
+  //   let name = outlet.activatedRouteData["name"];
+  //   if (name) return name;
+  //   else return -1;
+  // }
 }
